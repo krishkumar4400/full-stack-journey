80471728679bc59d446646a500d52350d4113b00
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-07-24 22:37:16 +0530

    feat: add HomePage component and configure routing with react-router

---
+++ 07-devops/01-docker-image/backend/server.js
@@ -0,0 +1,31 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
+app.get("/api/data", (req, res) => {
+  return res.status(200).json({
+    data: {
+      id: 1,
+      name: "Sample Data",
+      description: "This is a sample data response from the API",
+    },
+  });
+});
+
+app.get("/api/health", (req, res) => {
+  return res.status(200).json({
+    status: "OK",
+    timestamp: Date.now(),
+  });
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
+});
