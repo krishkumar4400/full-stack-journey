0f57c805dbafe3379359f239f32b4c9165bc1994
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-07-29 21:44:02 +0530

    Created package.json for frontend dependencies and scripts.
    Implemented main App component to fetch and display users from API.
    
    Configured Vite for development server with API proxy to backend.

--- 01-frontend/03-javascript/lecture-12/script.js
+++ 01-frontend/03-javascript/lecture-12/script.js
@@ -1,5 +1,32 @@
 /**
- * 1. create element
- * 2. appendChild
+ * 1. setTimeout
+ * 2. setInterval
  */
 
+let btn = document.querySelector("button");
+let para = document.querySelector("#progress");
+let inner = document.querySelector("#inner");
+let grow = 0;
+
+let x = Math.floor(Math.random() * 50);
+console.log(x);
+console.log(x + 50);
+
+btn.addEventListener("click", function () {
+  btn.style.pointerEvents = "none";
+  let interval = setInterval(() => {
+    grow++;
+    para.innerHTML = grow + " %";
+    inner.style.width = grow + "%";
+  }, x + 50);
+
+  setTimeout(
+    () => {
+      clearInterval(interval);
+      btn.innerHTML = "Downloaded";
+      btn.style.opacity = 0.5;
+      console.log("Downloaded in ", (x + 50) / 10, "Seconds");
+    },
+    (x + 50) * 100,
+  );
+});
