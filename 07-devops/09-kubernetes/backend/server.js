4e2d06efb7328a8e8bfb3fd155c3b0e310abf836
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-08-15 23:15:22 +0530

    Add backend Express application with Kubernetes deployment and service configuration
    - Created package.json for backend with Express and Morgan dependencies.
    - Implemented server.js to handle requests and perform a computation.
    - Added Kubernetes deployment configuration for the Express application.
    - Created Ingress resource for routing traffic to the application.
    - Defined a Service to expose the application within the cluster.

---
+++ 07-devops/09-kubernetes/backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  let sum = 0;
+  for (let i = 0; i < 1000000000; i++) {
+    sum += i;
+  }
+  res.status(200).json({
+    message: `Hello Express : Count = ${sum}`,
+    success: true,
+  });
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
+});
