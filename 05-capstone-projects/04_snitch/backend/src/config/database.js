7d05ff24e23f2f4f249194e80e023ed64b26bd88
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-09-03 22:36:29 +0530

    feat: add nodemailer configuration and utility files for API error handling and responses

--- 05-capstone-projects/04_snitch/backend/src/config/database.js
+++ 05-capstone-projects/04_snitch/backend/src/config/database.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
-import env from "./env";
+import env from "./env.js";
+import ApiError from "../utils/api-error.js";
 
 const connectToDB = async () => {
   try {
     await mongoose.connect(env.MONGO_URI);
+    console.log("Connected to Mongo DB");
   } catch (error) {
     console.error(error);
+    throw new ApiError(500, "Failed to connect with database", error);
   }
 };
 export default connectToDB;
